c71d8771d9184cdcfa5cb28319eb5fd017e4c841
bxrbeq <lelousta04@gmail.com>
2025-09-03 20:49:11 +0200

    removed backslashes

--- controllers/home.php
+++ controllers/home.php
@@ -8,4 +8,4 @@ $host = $_SERVER["HTTP_HOST"];
 $completeUri = $_SERVER["REQUEST_URI"];
 $link = "https://{$host}{$completeUri}";
 
-require base_path("views\\home.views.php");
+require base_path("views/home.views.php");
